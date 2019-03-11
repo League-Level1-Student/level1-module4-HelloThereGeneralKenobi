5d3dbe4d4c164a2ac8be9c9bb72973794374c030
varnit05 <League@AdministorsiMac.attlocal.net>
2019-03-10 17:25:26 -0700

    Finished Typing Tutor and looked at next one

--- src/TYPE_OR_DIE.java
+++ src/TYPE_OR_DIE.java
@@ -1,30 +1,71 @@
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class TYPE_OR_DIE {
+public class TYPE_OR_DIE implements KeyListener {
 	
 	static JFrame frame;
 	
-	JLabel label = new JLabel();
+	static JLabel label;
 	
-	J
-	
-	char currentLetter = generateRandomLetter(); 
-
-	char generateRandomLetter() {
-	      Random r = new Random();
-	      
-	      return (char) (r.nextInt(26) + 'a');
-	}
+	char currentLetter;
 	
 	public static void main(String[] args) {
 		TYPE_OR_DIE type = new TYPE_OR_DIE();
 		
+		type.setup();
+		
 	}
 	
 	void setup() {
+		currentLetter = generateRandomLetter();
 		frame = new JFrame("T Y P E   O R   D I E");
+		frame.setVisible(true);
+		label = new JLabel();
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		label.setText(currentLetter + "");
+		frame.add(label);
+		frame.setSize(200, 300);
+		frame.addKeyListener(this);
+	}
+	
+	char generateRandomLetter() {
+	      Random r = new Random();
+	      return (char) (r.nextInt(26) + 'a');
+	      
+}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyChar());
+		if(currentLetter == (e.getKeyChar())) {
+			frame.setBackground(Color.GREEN);
+			System.out.println("Correct");
+			label.setOpaque(true);
+			label.setBackground(Color.green);
+		} 
+		else {
+			frame.setBackground(Color.red);
+			label.setBackground(Color.red);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(currentLetter + "");
 	}
 }
